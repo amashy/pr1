a6a4eda977b5d019f04fcacc50f1e05354ac48b9
amashy <maxim041203@gmail.com>
2023-02-17 21:48:55 +0200

    first commit

---
+++ AnonBetween/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AnonBetween
+{
+    public delegate int AddDelegate(int a, int b, int c);
+    class Program
+    {
+        static void Main(string[] args)
+                           
+        {
+            Console.WriteLine("Введите 3 параметра:");
+
+            int val1 = Int32.Parse(Console.ReadLine());
+            int val2 = Int32.Parse(Console.ReadLine());
+            int val3 = Int32.Parse(Console.ReadLine());
+
+
+            AddDelegate aD = new AddDelegate(delegate
+                (int number1 , int number2 , int number3)
+            {
+                int result = (number1 + number2 + number3) / 3;
+
+                return result;
+            });
+
+            int serarif = aD(val1, val2, val3);
+
+            Console.WriteLine("Среднее арифметическое параметров = " + serarif);
+        }
+    }
+}
---
+++ lambdaarifmet/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+
+namespace lambdaarifmet
+{
+    public delegate int SomeDelegate (int a, int b);
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+            
+            Console.WriteLine("Value 1 = ");
+            int o = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Value 2 = ");
+            int b = Int32.Parse(Console.ReadLine());    
+
+            Console.WriteLine("'-' , '+' , '*' , '/'");
+
+            string action = Console.ReadLine();
+
+                switch (action)
+                {
+                    case "-":
+
+                        SomeDelegate Sub = (s, d) =>
+                        {
+                            int result = s - d;
+
+                            return result;
+                        };
+
+                        int c = Sub(o, b);
+
+                        Console.WriteLine(c);
+
+                        Console.ReadKey();
+                        break;
+
+                    case "+":
+
+                        SomeDelegate Add = (s, d) =>
+                        {
+                            int result = s + d;
+
+                            return result;
+                        };
+
+                        int q = Add(o, b);
+
+                        Console.WriteLine(q);
+
+                        Console.ReadKey();
+
+                        break;
+
+                    case "*":
+
+                        SomeDelegate Mul = (s, d) =>
+                        {
+                            int result = s * d;
+
+                            return result;
+                        };
+
+                        int j = Mul(o, b);
+
+                        Console.WriteLine(j);
+
+                        Console.ReadKey();
+
+                        break;
+                    case "/":
+
+                        if (b != 0)
+                        {
+
+                            SomeDelegate Div = (s, d) =>
+                            {
+                                int result = s / d;
+
+                                return result;
+                            };
+
+                            int k = Div(o, b);
+
+                            Console.WriteLine(k);
+
+                            Console.ReadKey();
+                        }
+                        else
+                        {
+                            Console.WriteLine("Только не 0");
+                        }
+
+                        break;
+                }
+            }
+                    
+        
+        }
+    }
+}
