e5b6ab375281ba7986cf7f1c5493a82e145be2a1
amashy <maxim041203@gmail.com>
2023-02-19 00:31:07 +0200

    last commit

---
+++ DelegateRandom/DelegateRandom/DelegateRandom/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+    public delegate int IntDelegate();
+
+    public static void Main()
+    {
+        IntDelegate[] delegates = new IntDelegate[5];
+        Random random = new Random();
+
+        for (int i = 0; i < delegates.Length; i++)
+        {
+            delegates[i] = () => random.Next(100);
+        }
+
+        double average = GetDelegateAverage(delegates);
+        Console.WriteLine($"Average: {average}");
+    }
+
+    public static double GetDelegateAverage(IntDelegate[] delegates)
+    {
+        int sum = 0;
+
+        foreach (IntDelegate del in delegates)
+        {
+            sum += del();
+        }
+             
+        return  (double)sum / delegates.Length;
+    }
+}
